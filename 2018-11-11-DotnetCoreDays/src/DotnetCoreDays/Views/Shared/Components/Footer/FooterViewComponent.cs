c03a631c859ea4e5652d3e8065ab8a01575d195e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-11-07 11:00:05 +0300

    Added footer view component

---
+++ 2018-11-11-DotnetCoreDays/src/DotnetCoreDays/Views/Shared/Components/Footer/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreDays.Views.Components.TodoCreateArea
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
