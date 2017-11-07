f035000c7542bedf572bfe72fdf74020c709d476
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-11-07 10:45:57 +0300

    Added EmailTagHelper

---
+++ 2018-11-11-DotnetCoreDays/src/DotnetCoreDays/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DotnetCoreDays.TagHelpers
+{
+    /// <summary>
+    /// Converts
+    ///           <email>info@sample.com</email>
+    /// To
+    ///           <a href="mailto:info@sample.com">info@sample.com</a>
+    /// </summary>
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var emailAddress = (await output.GetChildContentAsync()).GetContent();
+
+            output.Attributes.SetAttribute("href", $"mailto:{emailAddress}");
+            
+            output.Content.SetContent(emailAddress);
+        }
+    }
+}
