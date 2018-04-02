317468f1acd1e41945eea760a5470a2d051e1d62
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2018-04-02 11:12:17 +0300

    Added initial source code for multi-tenancy meeting.

---
+++ 2018-01-06-Multi-Tenancy/src/MultiTenancyDraft/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiTenancyDraft.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("OK");
+        }
+    }
+}
---
+++ 2018-01-06-Multi-Tenancy/src/MultiTenancyDraft/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiTenancyDraft
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddAuthentication()
+                .AddCookie();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
