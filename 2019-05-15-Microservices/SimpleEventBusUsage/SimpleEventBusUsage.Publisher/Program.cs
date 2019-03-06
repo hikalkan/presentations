f350f8f4055e5284606e8ac62a9246c64a4b83c8
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2019-03-06 13:43:31 +0300

    Added sample console solution to demonstrate messaging

---
+++ 2019-05-15-Microservices/SimpleEventBusUsage/SimpleEventBusUsage.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+
+namespace SimpleEventBusUsage.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<AppModule>(options =>
+            {
+                options.UseAutofac();
+            }))
+            {
+                application.Initialize();
+
+                var demo = application.ServiceProvider.GetService<PublisherDemo>();
+
+                demo.RunAsync().Wait();
+
+                application.Shutdown();
+            }
+        }
+    }
+}
---
+++ 2019-05-15-Microservices/SimpleEventBusUsage/SimpleEventBusUsage.Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.EventBus.Distributed;
+
+namespace SimpleEventBusUsage.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var application = AbpApplicationFactory.Create<AppModule>(options =>
+            {
+                options.UseAutofac();
+            }))
+            {
+                application.Initialize();
+
+                application.ServiceProvider.GetRequiredService<IDistributedEventBus>(); //TODO: Workaround, remove after ABP v0.15
+
+                Console.WriteLine("************* STARTED the SUBSCRIBER *************");
+                Console.WriteLine("Press ENTER to stop application...");
+                Console.ReadLine();
+
+                application.Shutdown();
+            }
+        }
+    }
+}
