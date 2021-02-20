a5d4c4662dec4ac584939e75c8081d4d246db85a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-02-20 19:31:59 +0300

    Prepare start case.

---
+++ 2021-02-21 DotnetKonf/src/KonfDemo_Final-old/KonfDemo.ServerApp/Entities/LikeRecord.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace KonfDemo.ServerApp.Entities
+{
+    public class LikeRecord : BasicAggregateRoot<string>
+    {
+        public int LikeCount { get; set; }
+        
+        public int DislikeCount { get; set; }
+
+        private LikeRecord()
+        {
+
+        }
+
+        public LikeRecord(string id)
+            : base(id)
+        {
+
+        }
+    }
+}
