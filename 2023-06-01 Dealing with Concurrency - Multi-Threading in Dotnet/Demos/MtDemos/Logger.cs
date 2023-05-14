2ba8e6ca496b5c85b3230ccb257b821627620d09
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-05-14 16:11:44 +0300

    Completed demo 1

--- 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/Logger.cs
+++ 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/Logger.cs
@@ -4,7 +4,7 @@
     {
         public static void Log(string message)
         {
-            Console.WriteLine($"[T {Thread.CurrentThread.ManagedThreadId:000}] {message}");
+            Console.WriteLine($"[T#{Thread.CurrentThread.ManagedThreadId:000}][{DateTime.Now:HH:mm:ss.fff}] {message}");
         }
     }
 }
