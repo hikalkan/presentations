93894058f2c3701f21d267f3d128372670fa0f1b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-05-15 16:03:25 +0300

    Update Program.cs

--- 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/Program.cs
+++ 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/Program.cs
@@ -5,12 +5,15 @@
         static void Main(string[] args)
         {
             //ThreadStartJoinDemo.Run();
+
             //MultipleThreadStartJoinDemo.Run();
+
             //ParameterizedMultipleThreadStartJoinDemo.Run();
+
             //SharingSimpleStateDemo.Run();
             //SharingSimpleStateDemo_Interlocked.Run();
             //SharingSimpleStateDemo_Lock.Run();
-            SharingSimpleStateDemo_Lock_Alternative.Run();
+            //SharingSimpleStateDemo_Lock_Alternative.Run();
         }
     }
 }
