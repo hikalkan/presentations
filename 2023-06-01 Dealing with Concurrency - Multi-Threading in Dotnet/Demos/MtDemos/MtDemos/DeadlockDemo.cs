c7668fd09f277f217ec38385a6eb7c8e276f3d5f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-05-21 18:06:56 +0300

    DeadlockDemo

---
+++ 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/MtDemos/DeadlockDemo.cs
@@ -0,0 +1,38 @@
+﻿namespace MtDemos
+{
+    public static class DeadlockDemo
+    {
+        private static object _lockA = new();
+        private static object _lockB = new();
+
+        public static void SomeMethod()
+        {
+            lock (_lockA)
+            {
+                // Some code...
+
+                lock (_lockB)
+                {
+                    // Some code...
+                }
+
+                // Some other code...
+            }
+        }
+
+        public static void AnotherMethod()
+        {
+            lock (_lockB)
+            {
+                // Some code...
+
+                lock (_lockA)
+                {
+                    // Some code...
+                }
+
+                // Some other code...
+            }
+        }
+    }
+}
