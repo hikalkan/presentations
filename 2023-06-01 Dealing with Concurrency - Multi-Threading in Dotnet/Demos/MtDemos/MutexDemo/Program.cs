3493ebda90aff91f3159f85d389dfc23db42b856
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-05-21 17:25:31 +0300

    Added mutex demo

---
+++ 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/MutexDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MutexDemo
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var mutex = new Mutex(false, "MutexDemo");
+
+            if (!mutex.WaitOne(100))
+            {
+                Console.WriteLine("Another instance of the application is already running. Exited!");
+                return -1;
+            }
+
+            Console.WriteLine("Application has started. Press <<ENTER>> to exit...");
+            Console.ReadLine();
+            
+            mutex.ReleaseMutex();
+            return 0;
+        }
+    }
+}
