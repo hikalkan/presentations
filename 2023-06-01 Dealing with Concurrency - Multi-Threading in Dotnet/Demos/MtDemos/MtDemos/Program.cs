3d7a0e9c919fa31dc7d8a7f54ab139476f459dda
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2023-05-28 10:25:02 +0300

    Remove some demos

--- 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/MtDemos/Program.cs
+++ 2023-06-01 Dealing with Concurrency - Multi-Threading in Dotnet/Demos/MtDemos/MtDemos/Program.cs
@@ -4,24 +4,13 @@
     {
         static async Task Main(string[] args)
         {
-            //ThreadStartJoinDemo.Run();
-
-            //MultipleThreadStartJoinDemo.Run();
-
             //ParameterizedMultipleThreadStartJoinDemo.Run();
 
             //SharingSimpleStateDemo.Run();
             //SharingSimpleStateDemo_Interlocked.Run();
             //SharingSimpleStateDemo_Lock.Run();
-            //SharingSimpleStateDemo_Lock_Alternative.Run();
-
-            //SharingSimpleStateDemo_Monitor.Run();
 
             //ProducerConsumerDemo.Run();
-
-            //AsyncCodeDemo.Run();
-            //SemaphoreDemo.Run();
-            //ThreadLocalDemo.Run();
         }
     }
 }
